5aedb79ed4788aa29164641316f23a91520d26cc
Yutong Chen <ytch@umich.edu>
2019-11-02 23:03:32 -0700

    add router

--- client/src/Components/List.js
+++ client/src/Components/List.js
@@ -4,6 +4,13 @@ import { makePage } from '../utilityFn/utility';
 import { NUM_PER_PAGE } from '../constants/todoConstant';
 import './list.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 export default class List extends Component {
   constructor(props) {
     super(props);
@@ -21,28 +28,38 @@ export default class List extends Component {
     let pages = makePage(total, cur, 2);
     return (
       <>
-        <ul className="ui middle aligned ordered selection list">
-          {todos.slice((cur - 1) * NUM_PER_PAGE, cur * NUM_PER_PAGE).map(todo => (
-            <ListItem
-              key={todo._id}
-              {...todo}
-              onClick={() => toggleTodo(todo._id)}
-            />
-          ))}
-        </ul>
-        <div className="pagination">
-          {pages.map((page, idx) => {
-            return page === '...' ?
-              <span className="ellipsis">{page}</span> :
-              <span
-                className="number"
-                key={idx}
-                onClick={() => this.handleClick(page)}>
-                {page}
-              </span>
-          }
-          )}
-        </div>
+        <Router>
+          <Switch>
+              <Route path={ cur === 1 ? '/' : `/${cur}`}>
+                <ul className="ui middle aligned ordered selection list">
+                  {todos.slice((cur - 1) * NUM_PER_PAGE, cur * NUM_PER_PAGE).map(todo => (
+                    <ListItem
+                      key={todo._id}
+                      {...todo}
+                      onClick={() => toggleTodo(todo._id)}
+                    />
+                  ))}
+                </ul>
+              </Route>
+            </Switch>
+          <div>
+            <div className="pagination">
+              {pages.map((page, idx) => {
+                return page === '...' ?
+                  <span className="ellipsis">{page}</span> :
+                  <Link to={{ pathname: page === 1 ? '/' : `/${page}` }}>
+                    <span
+                      className="number"
+                      key={idx}
+                      onClick={() => this.handleClick(page)}>
+                      {page}
+                    </span>
+                  </Link>
+              }
+              )}
+            </div>
+          </div>
+        </Router>
       </>
     )
   }
